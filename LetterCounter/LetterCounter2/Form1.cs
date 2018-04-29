b001ae46349fb54b3f22394443c4088afbc9abfe
totalintelli <blueintelli@gmail.com>
2018-04-29 22:10:37 +0900

    이 프로그램을 GUI(그래픽 사용자 인터페이스) 버전으로 작성하여 글자를 입력할 때마다 글자 수가 바로 바로 업데이트 되도록 함.

---
+++ LetterCounter/LetterCounter2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LetterCounter2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtInputString_TextChanged(object sender, EventArgs e)
+        {
+            // Initialize letterCount to 0
+            int letterCount = 0;
+
+            // Get letterCount from inputString
+            letterCount = txtInputString.Text.Length;
+
+            if (letterCount > 0)
+            {
+                // Display inputString + " has " + letterCount + " characters."
+                lbLetterCount.Text = txtInputString.Text + " has " + letterCount + " characters.";
+            }
+        }
+    }
+}
