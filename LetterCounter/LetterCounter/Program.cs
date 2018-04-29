5255f2cb4c660ec44efb073ccc05b031d32751e1
totalintelli <blueintelli@gmail.com>
2018-04-29 21:50:34 +0900

    문자열을 입력 받은 다음 입력 받은 문자열과 문자열의 글자수를 출력하는 프로그램을 작성.

---
+++ LetterCounter/LetterCounter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetterCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Letter Counter
+
+            // Initialize inputString to ""
+            string inputString = "";
+            // Initialize letterCount to 0
+            int letterCount = 0;
+
+            // Prompt for letter with "What is the input string?"
+            Console.Write("What is the input string?");
+            inputString = Console.ReadLine();
+
+            // Get letterCount from inputString
+            letterCount = inputString.Length;
+
+            // Display inputString + " has " + letterCount + " characters."
+            Console.WriteLine(inputString + " has " + letterCount + " characters.");
+
+            // End 
+            Console.ReadLine();
+        }
+    }
+}
